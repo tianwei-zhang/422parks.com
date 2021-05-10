374ad6af0e2ab5b4a952a2ee7aa19db2080c371f
Tianwei Zhang <kevinzhang122@gmail.com>
2021-05-09 17:30:18 -0700

    change site description

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,36 +2,36 @@ var store = [{
         "title": "3 Days Travel Guide For Tokyo",
         "excerpt":"TLDR/Itinerary Day 1: Hamarikyu Garden -&gt; Tsukiji Fish Market -&gt; Meiji Shrine Day 2: The Imperial Palace East Gardens -&gt; Asakusa Day 3: Shinjuku Gyoen National Garden -&gt; Shibuya -&gt; Akihabara In spite of its close location to China and Korea, Japan is unlike any other east asian countries. Its unique...","categories": [],
         "tags": ["City","Japan"],
-        "url": "http://www.422parks.com/3-Days-Travel-Guide-for-Tokyo/",
-        "teaser": "http://www.422parks.com/assets/img/japan/tokyo/Meiji%20Shrine%201.webp"
+        "url": "https://www.422parks.com/3-Days-Travel-Guide-for-Tokyo/",
+        "teaser": "https://www.422parks.com/assets/img/japan/tokyo/Meiji%20Shrine%201.webp"
       },{
         "title": "Nikko Travel Guide",
         "excerpt":"TLDR/Itinerary Must-do day trip if you are near Tokyo Plan a whole day to visit Shin-kyo -&gt; Rinno-ji -&gt; Tosho-gu -&gt; Shoyo-en The best time to visit is during fall season when leaves start to change color The Diary Nikko is a city one hour away from Tokyo. Nikko Tosho-gu...","categories": [],
         "tags": ["City","Japan"],
-        "url": "http://www.422parks.com/Nikko-Travel-Guide/",
-        "teaser": "http://www.422parks.com/assets/img/japan/nikko/Tosho-gu.webp"
+        "url": "https://www.422parks.com/Nikko-Travel-Guide/",
+        "teaser": "https://www.422parks.com/assets/img/japan/nikko/Tosho-gu.webp"
       },{
         "title": "Muri Woods Day Trip",
         "excerpt":"TLDR You need to reserve online before visit Plan to stay for 2 hours in the park The Diary Muri Woods National Monument is named after the legendary John Muri who is often known as the “Father of National Parks”. One hundred fifty million years ago ancestors of redwood and...","categories": [],
         "tags": ["National Parks","Parks"],
-        "url": "http://www.422parks.com/Muri-Woods-Day-Trip/",
-        "teaser": "http://www.422parks.com/assets/img/muri/muri_wood_1.webp"
+        "url": "https://www.422parks.com/Muri-Woods-Day-Trip/",
+        "teaser": "https://www.422parks.com/assets/img/muri/muri_wood_1.webp"
       },{
         "title": "Mount Diablo State Park",
         "excerpt":"TLDR: Mount Diablo is one of the highest points in San Francisco Bay Area Best time to visit is spring when hills are green Limited parking at the summit The Diary Mount Diablo is one of the highest points in San Francisco Bay Area. It is a great hiking spot...","categories": [],
         "tags": ["State Parks","Parks"],
-        "url": "http://www.422parks.com/Mount-Diablo-State-Park/",
-        "teaser": "http://www.422parks.com/assets/img/mt_diablo/mt%20diablo.webp"
+        "url": "https://www.422parks.com/Mount-Diablo-State-Park/",
+        "teaser": "https://www.422parks.com/assets/img/mt_diablo/mt%20diablo.webp"
       },{
         "title": "Crater Lake National Park Travel Guide",
         "excerpt":"TLDR Best to visit is summer due to snow and fog in other seasons Start your road trip from San Francisco and drive north on I-5 Prepare to spend one day at Crater Lake National Park Stay at the Crater Lake Lodge inside the park to save time from lining...","categories": [],
         "tags": ["National Parks","Parks"],
-        "url": "http://www.422parks.com/Crater-Lake-National-Park-Travel-Guide/",
-        "teaser": "http://www.422parks.com/assets/img/crater/crater%20lake%20national%20park.webp"
+        "url": "https://www.422parks.com/Crater-Lake-National-Park-Travel-Guide/",
+        "teaser": "https://www.422parks.com/assets/img/crater/crater%20lake%20national%20park.webp"
       },{
         "title": "2 Day Travel Guide For Grand Canyon National Park",
         "excerpt":"TLDR 1 Day Itinerary Scenic Option (e.g. photos, easy trail): Watch sunrise at Mather Point Walk the Rim Trail between Marther Point and Yavapai Point Lunch break Drive the Desert View Drive to visit different vistas (afternoon) Watch the sunset at Yavapai Point or Pipe Creek Vista Outdoor Option (e.g....","categories": [],
         "tags": ["National Parks","Parks"],
-        "url": "http://www.422parks.com/2-Day-Travel-Guide-for-Grand-Canyon-National-Park/",
-        "teaser": "http://www.422parks.com/assets/img/grand_canyon/mather%20point%20grand%20canyon%20sunrise.webp"
+        "url": "https://www.422parks.com/2-Day-Travel-Guide-for-Grand-Canyon-National-Park/",
+        "teaser": "https://www.422parks.com/assets/img/grand_canyon/mather%20point%20grand%20canyon%20sunrise.webp"
       }]
